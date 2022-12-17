8122ed401f35318b597f9aac56d17ccbf9593107
gallegodev <gallegodev1991@gmail.com>
2022-12-17 18:31:00 +0000

    Add more global variables

--- assets/java script/script.js
+++ assets/java script/script.js
@@ -2,5 +2,8 @@ let userScore = 0;
 let computerScore = 0;
 let userScore_span = document.getElementById("user-score");
 let computerScore_span = document.getElementById("computer-score");
-
-
+let scoreBoard_div = document.querySelector(".score-board");
+let result_div = document.querySelector(".result");
+let rock_div = document.getElementById("rock");
+let paper_div = document.getElementById("paper");
+let scissors_div = document.getElementById("scissors");
