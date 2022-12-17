5b637f654a87ba4e292a2601c2ba78d3392cd252
gallegodev <gallegodev1991@gmail.com>
2022-12-17 18:50:38 +0000

    Add event listeners to game buttons

--- assets/java script/script.js
+++ assets/java script/script.js
@@ -1,3 +1,5 @@
+/**Global variables */
+
 let userScore = 0;
 let computerScore = 0;
 let userScore_span = document.getElementById("user-score");
@@ -8,7 +10,16 @@ let rock_div = document.getElementById("rock");
 let paper_div = document.getElementById("paper");
 let scissors_div = document.getElementById("scissors");
 
+/**Buttons Functions */
 
 rock_div.addEventListener('click', function() {
     console.log("You are pressing the Rock!!!")
+})
+
+paper_div.addEventListener('click', function() {
+    console.log("You are pressing the Paper!!!")
+})
+
+scissors_div.addEventListener('click', function() {
+    console.log("You are pressing the Scissors!!!")
 })
